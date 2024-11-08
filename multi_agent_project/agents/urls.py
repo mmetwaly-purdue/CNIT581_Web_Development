fcd86d8f01bd2e6a4707a1054d429b6a98203e21
mmetwaly-purdue <mmetwaly@purdue.edu>
2024-11-08 15:40:12 -0500

    Changed src line syntax to match Django

--- multi_agent_project/agents/urls.py
+++ multi_agent_project/agents/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import register, signin, store_connection
+from .views import register, signin, store_connection, upload_document 
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
@@ -12,7 +12,7 @@ urlpatterns = [
     path('create/', views.create_page, name='create_page'),
     path('create_agent/', views.create_agent, name='create_agent'),
     path('delete_agent/', views.delete_agent, name='delete_agent'),
-    path('upload_document/', views.upload_document, name='upload_document'),
+    path('upload_document/', upload_document, name='upload_document'),
     path('workflow/', views.workflow_page, name='workflow_page'),
     path('register/', register, name='register'),
     path('signin/', signin, name='signin'),
